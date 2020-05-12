8ea33425610773d6958ba15a71d40117b62b9387
harelgross <harelgross@gmail.com>
2020-05-12 09:56:08 +0300

    Added fixed navbar

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const progressBar = document.body.querySelector("#progress");
-
-document.addEventListener('scroll',function() {
-    const distanceTop = window.pageYOffset;
-    const pageHeight = document.body.scrollHeight - window.innerHeight;
-
-    progressBar.style.width = 100 * (distanceTop / pageHeight) + '%';
-    console.log('Works in Chrome!');
-   
-});
+window.addEventListener('hashchange', function() {scrollBy(0, -140);});
